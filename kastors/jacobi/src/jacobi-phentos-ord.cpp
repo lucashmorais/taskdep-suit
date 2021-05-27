3cf89db431633ae5f09e27e78e1bf55b763e67ef
Lucas Morais <morais.lucas.h@gmail.com>
2021-05-27 17:16:16 -0300

    Continue searching for a stable design point for jacobi-phentos-ord

--- kastors/jacobi/src/jacobi-phentos-ord.cpp
+++ kastors/jacobi/src/jacobi-phentos-ord.cpp
@@ -61,8 +61,38 @@ void ORD_sweep_partial_b(uint64_t swID) {
 	}
 }
 
+void sweep(int nx, int ny, double dx, double dy, double *f_,
+        int itold, int itnew, double *u_, double *unew_)
+{
+    int i;
+    int it;
+    int j;
+    double (*f)[nx][ny] = (double (*)[nx][ny])f_;
+    double (*u)[nx][ny] = (double (*)[nx][ny])u_;
+    double (*unew)[nx][ny] = (double (*)[nx][ny])unew_;
+
+    for (it = itold + 1; it <= itnew; it++) {
+        for (i = 0; i < nx; i++) {
+            for (j = 0; j < ny; j++) {
+                (*u)[i][j] = (*unew)[i][j];
+            }
+        }
+        for (i = 0; i < nx; i++) {
+            for (j = 0; j < ny; j++) {
+                if (i == 0 || j == 0 || i == nx - 1 || j == ny - 1) {
+                    (*unew)[i][j] = (*f)[i][j];
+                } else {
+                    (*unew)[i][j] = 0.25 * ((*u)[i-1][j] + (*u)[i][j+1]
+                            + (*u)[i][j-1] + (*u)[i+1][j]
+                            + (*f)[i][j] * dx * dy);
+                }
+            }
+        }
+    }
+}
+
 /* #pragma omp task depend version of SWEEP. */
-void sweep (int nx_, int ny_, double dx_, double dy_, double *f__,
+void sweep_parallel (int nx_, int ny_, double dx_, double dy_, double *f__,
         int itold, int itnew, double *u__, double *unew__, int block_size)
 {
     int i;
